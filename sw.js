7e001ca4c375332c2d8c022ca811ed994bc9ce3c
corone <fxfx_pot@yahoo.co.jp>
2024-09-10 13:00:27 +0900

    fix ServiceWorker

--- sw.js
+++ sw.js
@@ -4,33 +4,39 @@ const MAIN_CACHE = 'main_20240910';
 self.addEventListener("install", async (event) => {
     event.waitUntil((async () => {
         const cache = await caches.open(MAIN_CACHE)
-        await cache.addAll(['.'])
+        await cache.addAll([
+            '.',
+            './checkbox.css',
+            './favicon.ico',
+            './index.html',
+            './manifest.webmanifest',
+            './guided_audio/exhale_en.mp3',
+            './guided_audio/exhale_ja.mp3',
+            './guided_audio/finish_en.mp3',
+            './guided_audio/finish_ja.mp3',
+            './guided_audio/hold_en.mp3',
+            './guided_audio/hold_ja.mp3',
+            './guided_audio/inhale_en.mp3',
+            './guided_audio/inhale_ja.mp3',
+            './icons/icon_64.png',
+            './icons/icon_180.png',
+            './icons/icon_256.png',
+            './icons/icon_512.png'
+        ])
     })())
 });
   
-const cacheFirst = (event) => {
-
-    if (event.request.method.toLowerCase() != 'get') return
-    
-    const requestUrl = new URL(event.request.url)
-    
-    if (requestUrl.protocol != 'http:' && requestUrl.protocol != 'https:') return;
-
-    event.respondWith((async ()=>{
-        const cache = await caches.open(MAIN_CACHE)
-
-        const responsePromise = fetch(event.request).then((response) => {
-            if (response.ok) cache.put(event.request, response.clone())
-            return response
-        })
-
-        let cacheResponse = await caches.match(event.request)
-
-        return cacheResponse || await responsePromise
-    })())
+const cacheFirst = async (request) => {
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+        return responseFromCache;
+    }
+    return fetch(request);
 };
 
-self.addEventListener('fetch', cacheFirst);
+self.addEventListener("fetch", (event) => {
+    event.respondWith(cacheFirst(event.request));
+});
 
 const deleteCache = async (key) => {
     await caches.delete(key);
